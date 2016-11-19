027baeac7c651e844aefe1fee059796ba54f7b40
jasydong <jasydong@qq.com>
2016-11-18 19:09:31 -0800

    更新测试文件(添加异常处理)

--- test.php
+++ test.php
@@ -1,5 +1,29 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", true);
+
+//注册异常处理
+register_shutdown_function(function() {
+	print_r(error_get_last());
+});
+
+set_error_handler(function($code, $error, $file = null, $line = null) {
+	if (error_reporting() & $code) {
+		debug_print_backtrace();
+        return true;
+	}
+
+	return true;
+});
+
+set_exception_handler(function($e) {
+	if ($e)	{
+		print_r($e);
+        return true;
+	}
+});
+
 //自动加载注册
 spl_autoload_register(function($class) {
 	//自动加载文件路径
