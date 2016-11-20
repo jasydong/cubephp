05de5e6686634b1abda44fe4ed4d8cdb17b3e5f5
jasydong <jasydong@qq.com>
2016-11-19 18:05:12 -0800

    添加Model目录

---
+++ model/demo.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Model;
+
+/**
+ * Demo Model
+ *
+ * @package    Model
+ * @author     JasyDong
+ * @copyright  (c) 2016 CubePHP
+ */
+class Demo {
+
+	/**
+	 * Index Action
+	 */
+	public function demo()
+	{
+		print_r('Hello, Model!');
+	}
+
+}
