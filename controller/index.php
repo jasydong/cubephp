33b01e741a87278233f8124581b1ec8f37a00ac9
jasydong <jasydong@qq.com>
2016-11-19 17:56:48 -0800

    添加控制器目录

---
+++ controller/index.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Controller;
+
+/**
+ * Index Controller
+ *
+ * @package    Controller
+ * @author     JasyDong
+ * @copyright  (c) 2016 CubePHP
+ */
+class Index {
+
+	/**
+	 * Index Action
+	 */
+	public function index()
+	{
+		print_r('hello, world!');
+	}
+
+}
