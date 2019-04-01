91fac5aa0d9539f7be4c50e6c242fc4aea2c0a6f
Jasy Dong <jasydong@qq.com>
2019-04-01 15:34:15 +0800

    删除view目录

--- view/index/index.php
+++
@@ -1,6 +0,0 @@
-<h3>Hello, CubePHP</h3>
-<p>Index::Index</p>
-<h3>Debug Stack:</h3>
-<p><pre><?php debug_print_backtrace(); ?></pre></p>
-<h3>Included Files:</h3>
-<p><pre><?php print_r(get_included_files()); ?></pre></p>
