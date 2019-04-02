11109fce33a8a5685787134793f6198625c3f067
Jasy Dong <jasydong@qq.com>
2019-04-02 18:15:09 +0800

    添加Mysql类

---
+++ src/Core/Mysql.php
@@ -0,0 +1,15 @@
+<?php
+namespace CubePHP\Core;
+
+use Workerman\MySQL\Connection;
+
+/**
+ * Mysql Class
+ *
+ * @package    Core
+ * @author     JasyDong
+ * @copyright  (c) 2019 CubePHP
+ */
+class Mysql extends Connection {
+
+}
